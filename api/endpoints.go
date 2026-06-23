c458dab17851ffc25072f132b11e786fe10f1110
Siroko-0516 <jisung.20090725@gmail.com>
2026-06-23 11:45:31 +0900

    Update endpoints.go

--- api/endpoints.go
+++ api/endpoints.go
@@ -338,8 +338,10 @@ func handleUpdateAll(w http.ResponseWriter, r *http.Request) {
 			httpError(w, r, fmt.Errorf("failed to retrieve playtime: %s", err), http.StatusInternalServerError)
 			return
 		}
-	} else {
-		playtime, ok := data.System.GameStats.(map[string]interface{})["playTime"].(float64)
+	//} else {
+	} else if data.System.GameStats != nil && oldSystem.GameStats != nil {
+    	playtime, ok := data.System.GameStats.(map[string]interface{})["playTime"].(float64)
+		//playtime, ok := data.System.GameStats.(map[string]interface{})["playTime"].(float64)
 		if !ok {
 			httpError(w, r, fmt.Errorf("no playtime found"), http.StatusBadRequest)
 			return
